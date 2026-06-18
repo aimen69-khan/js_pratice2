827aa54cd785839a27d8845dd781d2601b166264
Ahmed Raza <aimen69khan@gmail.com>
2026-06-18 12:58:46 +0500

    JS pratice var let const

---
+++ app.js
@@ -0,0 +1,35 @@
+var a = 6;
+var a = 5;
+
+// What do u think the output in console.log
+
+console.log(a);
+
+// if u said 5 you are correct if U said 6 u are wrong
+// because var can be redeclard and reassigned
+
+
+
+// but in let the story is different
+
+let b = 3;
+
+// let b = 4;
+
+b = 5;
+
+console.log(b);
+
+// in let u can reassign it u can't redeclared and the answer is 5
+
+
+
+// in const is very different story
+
+const c = 23;
+// c = 89;
+
+console.log(c);
+
+// in const u can't reassign or redeclared it the value is fixed
+
